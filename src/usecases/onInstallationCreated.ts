dd6947796015e790a3b3b6421c9e717bcff8af7d
CHISEN Kaoru <cordx56@cordx.cx>
2025-08-29 12:05:15 +0900

    fix: 環境変数チェック

--- src/usecases/onInstallationCreated.ts
+++ src/usecases/onInstallationCreated.ts
@@ -24,40 +24,49 @@ export async function onInstallationCreated(
       const repoName = repo.name;
       const branchName = "hako/configure";
 
+      if (
+        !process.env.SAKURACR_REGISTRY ||
+        !process.env.SAKURACR_USER ||
+        !process.env.SAKURACR_PASSWORD ||
+        !process.env.SAKURA_API_TOKEN ||
+        !process.env.SAKURA_API_SECRET
+      ) {
+        throw new Error("Sakura envs missing!");
+      }
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURACR_REGISTRY",
-        process.env.SAKURACR_REGISTRY!,
+        process.env.SAKURACR_REGISTRY,
       );
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURACR_USER",
-        process.env.SAKURACR_USER!,
+        process.env.SAKURACR_USER,
       );
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURACR_PASSWORD",
-        process.env.SAKURACR_PASSWORD!,
+        process.env.SAKURACR_PASSWORD,
       );
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURA_API_TOKEN",
-        process.env.SAKURA_API_TOKEN!,
+        process.env.SAKURA_API_TOKEN,
       );
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURA_API_SECRET",
-        process.env.SAKURA_API_SECRET!,
+        process.env.SAKURA_API_SECRET,
       );
 
       await createBranch(octokit, owner, repoName, branchName);
