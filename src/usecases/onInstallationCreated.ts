4e33b869a961f3940137492ea454bdadaa05cf44
CHISEN Kaoru <cordx56@cordx.cx>
2025-08-29 12:39:10 +0900

    fix: remove enc assert

--- src/usecases/onInstallationCreated.ts
+++ src/usecases/onInstallationCreated.ts
@@ -29,35 +29,35 @@ export async function onInstallationCreated(
         owner,
         repoName,
         "SAKURACR_REGISTRY",
-        process.env.SAKURACR_REGISTRY,
+        process.env.SAKURACR_REGISTRY!,
       );
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURACR_USER",
-        process.env.SAKURACR_USER,
+        process.env.SAKURACR_USER!,
       );
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURACR_PASSWORD",
-        process.env.SAKURACR_PASSWORD,
+        process.env.SAKURACR_PASSWORD!,
       );
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURA_API_TOKEN",
-        process.env.SAKURA_API_TOKEN,
+        process.env.SAKURA_API_TOKEN!,
       );
       await createSecret(
         octokit,
         owner,
         repoName,
         "SAKURA_API_SECRET",
-        process.env.SAKURA_API_SECRET,
+        process.env.SAKURA_API_SECRET!,
       );
 
       await createBranch(octokit, owner, repoName, branchName);
